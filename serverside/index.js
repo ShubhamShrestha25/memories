31d28b39bdeb2babb85d4c2803ae6c60fb658d85
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-03-28 09:34:09 +0545

    basic backend work done!

--- serverside/index.js
+++ serverside/index.js
@@ -1,10 +1,28 @@
-import bodyParser from "body-parser"
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
-app.use(cors())
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const COLLECTION_URL =
+  "mongodb+srv://shubham25:nepal123@cluster0.owhdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(COLLECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
