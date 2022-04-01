e402d59c3524c82bdaa42e1956713a5860f5a999
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-04-01 18:54:04 +0545

    form complete

--- serverside/index.js
+++ serverside/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const COLLECTION_URL =
   "mongodb+srv://shubham25:nepal123@cluster0.owhdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
